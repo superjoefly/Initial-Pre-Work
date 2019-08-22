7994d598fb37e43d100b9cd05e27248d9c0eafaa
JoeFly <superjoefly@gmail.com>
2019-08-22 12:33:49 -0600

    Learned about Variable Case Sensitivity

---
+++ 5-Introduction-to-JavaScript/6-Understanding-Case-Sensitivity-in-Variables.js
@@ -0,0 +1,12 @@
+
+// Learned about Variable Case Sensitivity
+
+// Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
