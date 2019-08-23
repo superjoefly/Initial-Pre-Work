c37bd1d7241d56f9d8aa95043b3da37c44d7a0e0
JoeFly <superjoefly@gmail.com>
2019-08-23 17:41:41 -0600

    Learned how to Append Variables Using Strings

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,13 @@
+
+// Learned how to Append Variables Using Strings
+
+// Example
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+
+// Only change code below this line
+
+var someAdjective = "awesome!";
+var myStr = "Learning to code is ";
+myStr += someAdjective;
