d9b5453c1acecae4fe4dd9312d9869239c461c9d
JoeFly <superjoefly@gmail.com>
2019-08-23 17:38:36 -0600

    Learned how to Construct Strings with Variables

---
+++ 5-Introduction-to-JavaScript/26-Concatenating_Strings_with_the_Plus_Equals_Operator.js
@@ -0,0 +1,10 @@
+
+// Learned how to Construct Strings with Variables
+
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Joseph Atwood";
+var myStr = "My name is " + myName + " and I am well!";
