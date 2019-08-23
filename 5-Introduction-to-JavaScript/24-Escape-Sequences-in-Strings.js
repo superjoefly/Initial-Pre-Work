13b36819d330fcc05e190ee46b2157117d2f74f4
JoeFly <superjoefly@gmail.com>
2019-08-23 17:35:03 -0600

    Learned how to Concatenate Strings with Plus Operator

---
+++ 5-Introduction-to-JavaScript/24-Escape-Sequences-in-Strings.js
@@ -0,0 +1,9 @@
+
+// Learned how to Concatenate Strings with Plus Operator
+
+// Example
+var ourStr = "I come first. " + "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the start. " + "This is the end.";
