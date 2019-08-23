bdd2814b9b081f3e1abee3bd29677c4f525bba0a
JoeFly <superjoefly@gmail.com>
2019-08-23 17:36:55 -0600

    Learned how to Concatenate Strings with the Plus Equals Operator

---
+++ 5-Introduction-to-JavaScript/25-Concatenating_Strings_with_Plus_Operator.js
@@ -0,0 +1,12 @@
+
+// Learned how to Concatenate Strings with the Plus Equals Operator
+
+// Example
+var ourStr = "I come first. ";
+ourStr += "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the first sentence. ";
+
+myStr += "This is the second sentence.";
