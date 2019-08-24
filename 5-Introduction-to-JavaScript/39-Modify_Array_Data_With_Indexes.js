68269b73055919fd2e359157d8a1557ed0a7f47d
JoeFly <superjoefly@gmail.com>
2019-08-23 18:25:26 -0600

    Learned how to Modify Array Data with Indexes

---
+++ 5-Introduction-to-JavaScript/39-Modify_Array_Data_With_Indexes.js
@@ -0,0 +1,13 @@
+
+// Learned how to Modify Array Data with Indexes
+
+// Example
+var ourArray = [18,64,99];
+ourArray[1] = 45; // ourArray now equals [18,45,99].
+
+// Setup
+var myArray = [18,64,99];
+
+// Only change code below this line.
+
+myArray[0] = 45;
