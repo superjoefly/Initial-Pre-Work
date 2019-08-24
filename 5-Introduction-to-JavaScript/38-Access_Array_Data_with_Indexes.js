33d609d3bb10ae6d88946382d3606bd6302648dc
JoeFly <superjoefly@gmail.com>
2019-08-23 18:24:26 -0600

    Learned how to Access Array Data with Indexes

---
+++ 5-Introduction-to-JavaScript/38-Access_Array_Data_with_Indexes.js
@@ -0,0 +1,13 @@
+
+// Learned how to Access Array Data with Indexes
+
+// Example
+var ourArray = [50,60,70];
+var ourData = ourArray[0]; // equals 50
+
+// Setup
+var myArray = [50,60,70];
+
+// Only change code below this line.
+
+var myData = myArray[0];
