a22ea7a5c8fc3f12c637167eab994cff99a18960
JoeFly <superjoefly@gmail.com>
2019-08-23 18:20:13 -0600

    Learned about Word Blanks

---
+++ 5-Introduction-to-JavaScript/35-Word_Blanks.js
@@ -0,0 +1,14 @@
+
+// Learned about Word Blanks
+
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  // Your code below this line
+
+  var result = "My " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb;
+
+  // Your code above this line
+  return result;
+}
+
+// Change the words here to test your function
+wordBlanks("dog", "big", "ran", "quickly");
