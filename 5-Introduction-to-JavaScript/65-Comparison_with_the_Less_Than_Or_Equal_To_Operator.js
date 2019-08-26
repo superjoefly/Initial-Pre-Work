ce51c0ae9cfff5ae2ba83e067bae656243f89403
JoeFly <superjoefly@gmail.com>
2019-08-26 17:49:01 -0600

    Learned about Comparison with the Less Than or Equal To Operator

---
+++ 5-Introduction-to-JavaScript/65-Comparison_with_the_Less_Than_Or_Equal_To_Operator.js
@@ -0,0 +1,17 @@
+
+// Learned about Comparison with the Less Than or Equal To Operator
+
+function testLessOrEqual(val) {
+  if (val <= 12) {  // Change this line
+    return "Smaller Than or Equal to 12";
+  }
+
+  if (val <= 24) {  // Change this line
+    return "Smaller Than or Equal to 24";
+  }
+
+  return "More Than 24";
+}
+
+// Change this value to test
+testLessOrEqual(10);
