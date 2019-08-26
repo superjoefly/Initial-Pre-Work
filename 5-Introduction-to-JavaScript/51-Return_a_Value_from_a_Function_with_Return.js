4a80694a092ab90e598caa6f9a5fbda3b85bd25d
JoeFly <superjoefly@gmail.com>
2019-08-26 17:14:20 -0600

    Learned how to Return a Value from a Function with 'return'

---
+++ 5-Introduction-to-JavaScript/51-Return_a_Value_from_a_Function_with_Return.js
@@ -0,0 +1,17 @@
+
+// Learned how to Return a Value from a Function with 'return'
+
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+
+function timesFive(arg1) {
+  return arg1 * 5;
+}
+
+console.log(minusSeven(10));
+
+console.log(timesFive(3));
