49a5f44cad9b0d6e282e4f853b10fe60a4be64d2
JoeFly <superjoefly@gmail.com>
2019-08-26 17:36:43 -0600

    Learned about Comparison with the Equality Operator

---
+++ 5-Introduction-to-JavaScript/57-Comparison_with_the_Equality_Operator.js
@@ -0,0 +1,13 @@
+
+// Learned about Comparison with the Equality Operator
+
+// Setup
+function testEqual(val) {
+  if (val == 12) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+testEqual(12);
