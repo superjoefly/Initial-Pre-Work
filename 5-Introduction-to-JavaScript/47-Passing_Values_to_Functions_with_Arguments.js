1ac5e7dbaf1b87cd2be93b40cb743346113a3b6a
JoeFly <superjoefly@gmail.com>
2019-08-26 17:03:54 -0600

    Learned to Pass Values to Functions with Arguments

---
+++ 5-Introduction-to-JavaScript/47-Passing_Values_to_Functions_with_Arguments.js
@@ -0,0 +1,16 @@
+
+// Learned to Pass Values to Functions with Arguments
+
+// Example
+function ourFunctionWithArgs(a, b) {
+  console.log(a - b);
+}
+ourFunctionWithArgs(10, 5); // Outputs 5
+
+// Only change code below this line.
+
+function functionWithArgs(arg1, arg2) {
+  console.log(arg1 + arg2);
+}
+
+functionWithArgs(2, 3);
