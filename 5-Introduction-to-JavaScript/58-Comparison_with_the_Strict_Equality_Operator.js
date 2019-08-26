3f8f31dcf12146510bed576d86474c58b3333055
JoeFly <superjoefly@gmail.com>
2019-08-26 17:38:15 -0600

    Learned about Comparison using the Strict Equality Operator

---
+++ 5-Introduction-to-JavaScript/58-Comparison_with_the_Strict_Equality_Operator.js
@@ -0,0 +1,13 @@
+
+// Learned about Comparison using the Strict Equality Operator
+
+// Setup
+function testStrict(val) {
+  if (val === 7) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+testStrict(7);
