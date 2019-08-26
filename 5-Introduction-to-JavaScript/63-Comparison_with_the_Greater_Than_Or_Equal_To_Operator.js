5e6be9a22617b60010cb795a866c1b2310d2cd23
JoeFly <superjoefly@gmail.com>
2019-08-26 17:46:52 -0600

    Learned about Comparison with the Greater Than or Equal To Operator

---
+++ 5-Introduction-to-JavaScript/63-Comparison_with_the_Greater_Than_Or_Equal_To_Operator.js
@@ -0,0 +1,17 @@
+
+// Learned about Comparison with the Greater Than or Equal To Operator
+
+function testGreaterOrEqual(val) {
+  if (val >= 20) {  // Change this line
+    return "20 or Over";
+  }
+
+  if (val >= 10) {  // Change this line
+    return "10 or Over";
+  }
+
+  return "Less than 10";
+}
+
+// Change this value to test
+testGreaterOrEqual(10);
