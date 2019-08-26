3d57b16ab649bde209e7d0f2a91891e0ea022d3d
JoeFly <superjoefly@gmail.com>
2019-08-26 17:42:37 -0600

    Learned about Comparison with the Strict Inequality Operator

---
+++ 5-Introduction-to-JavaScript/61-Comparison_with_the_Strict_Inequality_Operator.js
@@ -0,0 +1,18 @@
+
+// Learned about Comparison with the Strict Inequality Operator
+
+// Setup
+function testStrictNotEqual(val) {
+  // Only Change Code Below this Line
+
+  if (val !== 17) {
+
+  // Only Change Code Above this Line
+
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testStrictNotEqual("17");
