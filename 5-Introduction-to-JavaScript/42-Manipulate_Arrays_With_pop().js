fb878e03113b097f49156a0e2c9693aa834a5e55
JoeFly <superjoefly@gmail.com>
2019-08-26 16:50:02 -0600

    Learned how to Manipulate Arrays with 'pop()'

---
+++ 5-Introduction-to-JavaScript/42-Manipulate_Arrays_With_pop().js
@@ -0,0 +1,15 @@
+
+// Learned how to Manipulate Arrays with 'pop()'
+
+// Example
+var ourArray = [1,2,3];
+var removedFromOurArray = ourArray.pop();
+// removedFromOurArray now equals 3, and ourArray now equals [1,2]
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line.
+var removedFromMyArray;
+
+removedFromMyArray = myArray.pop(1);
