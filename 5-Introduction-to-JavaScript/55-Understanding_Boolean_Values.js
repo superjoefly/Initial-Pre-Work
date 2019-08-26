352a6d02fdfbe428608e67063816ffb191434651
JoeFly <superjoefly@gmail.com>
2019-08-26 17:30:15 -0600

    Learned about Boolean Values

---
+++ 5-Introduction-to-JavaScript/55-Understanding_Boolean_Values.js
@@ -0,0 +1,11 @@
+
+// Learned about Boolean Values
+
+function welcomeToBooleans() {
+
+// Only change code below this line.
+
+return true; // Change this line
+
+// Only change code above this line.
+}
