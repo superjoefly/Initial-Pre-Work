216007404b22472f6e229a243207b72c589498fb
JoeFly <superjoefly@gmail.com>
2019-08-26 16:54:44 -0600

    Learned how to Manipulate Arrays with 'unshift()'

---
+++ 5-Introduction-to-JavaScript/44-Manipulate_Arrays_With_unshift().js
@@ -0,0 +1,19 @@
+
+// Learned how to Manipulate Arrays with 'unshift()'
+
+
+// Example
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.shift(); // ourArray now equals ["J", "cat"]
+ourArray.unshift("Happy");
+// ourArray now equals ["Happy", "J", "cat"]
+
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+
+// Only change code below this line.
+
+myArray.unshift(["Paul", 35]);
+
+console.log(myArray);
