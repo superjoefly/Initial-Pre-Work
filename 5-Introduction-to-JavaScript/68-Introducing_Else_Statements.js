554d9172671e4fe9dd387f7806363385006e4e0f
JoeFly <superjoefly@gmail.com>
2019-08-26 18:38:11 -0600

    Learned about Else Statements

---
+++ 5-Introduction-to-JavaScript/68-Introducing_Else_Statements.js
@@ -0,0 +1,19 @@
+
+// Learned about Else Statements
+
+function testElse(val) {
+  var result = "";
+  // Only change code below this line
+
+  if (val > 5) {
+    result = "Bigger than 5";
+  } else {
+    return "5 or Smaller";
+  }
+
+  // Only change code above this line
+  return result;
+}
+
+// Change this value to test
+testElse(4);
