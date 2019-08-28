6e7ba1aaceed96b62273665ac63c348e722c2bb7
JoeFly <superjoefly@gmail.com>
2019-08-28 08:36:10 -0600

    Learned how to Build JS Objects

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,23 @@
+
+// Learned how to Build JS Objects
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  "name": "Bella",
+  "legs": 4,
+  "tails": 1,
+  "friends": [
+    "Stella",
+    "Cody",
+    "Fido"
+  ]
+};
