a22b688dd3aa6fac7cbcc2c76b76e05e0041db34
JoeFly <superjoefly@gmail.com>
2019-08-28 07:58:35 -0600

    Learned how to Select from Many Options with Switch Statements

---
+++ 5-Introduction-to-JavaScript/73-Selecting_from_Many_Options_with_Switch_Statements.js
@@ -0,0 +1,27 @@
+
+// Learned how to Select from Many Options with Switch Statements
+
+function caseInSwitch(val) {
+  var answer = "";
+  // Only change code below this line
+
+  switch(val) {
+    case 1:
+      answer = "alpha";
+      break;
+    case 2:
+      answer = "beta";
+      break;
+    case 3:
+      answer = "gamma";
+      break;
+    case 4:
+      answer = "delta";
+  }
+
+  // Only change code above this line
+  return answer;
+}
+
+// Change this value to test
+caseInSwitch(1);
