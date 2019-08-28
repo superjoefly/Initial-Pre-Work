d87b97efd0d1833d0bacc06d3c1fb937cc2a4e37
JoeFly <superjoefly@gmail.com>
2019-08-28 09:07:34 -0600

    Learned how to Manipulate Complex Objects

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,27 @@
+
+// Learned how to Manipulate Complex Objects
+
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  // Add record here
+  {
+    "artist": "Lane 8",
+    "title": "Spring Mix",
+    "release_year": 2019,
+    "formats": [
+      "mp3",
+      "m4a",
+      "ogg"
+    ]
+  }
+];
