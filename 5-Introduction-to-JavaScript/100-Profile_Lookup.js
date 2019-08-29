6447aa8dc39096cdd58982a2b4fd6cfd66205d25
superjoefly <superjoefly@gmail.com>
2019-08-29 16:32:06 -0600

    Working on Profile Lookup Challenge

---
+++ 5-Introduction-to-JavaScript/100-Profile_Lookup.js
@@ -0,0 +1,56 @@
+
+// Completed Profile Lookup Challenge
+
+//Setup
+var contacts = [
+    {
+        "firstName": "Akira",
+        "lastName": "Laine",
+        "number": "0543236543",
+        "likes": ["Pizza", "Coding", "Brownie Points"]
+    },
+    {
+        "firstName": "Harry",
+        "lastName": "Potter",
+        "number": "0994372684",
+        "likes": ["Hogwarts", "Magic", "Hagrid"]
+    },
+    {
+        "firstName": "Sherlock",
+        "lastName": "Holmes",
+        "number": "0487345643",
+        "likes": ["Intriguing Cases", "Violin"]
+    },
+    {
+        "firstName": "Kristian",
+        "lastName": "Vos",
+        "number": "unknown",
+        "likes": ["JavaScript", "Gaming", "Foxes"]
+    }
+];
+
+
+function lookUpProfile(name, prop){
+// Only change code below this line
+
+// Loop through the contacts:
+for (var i = 0; i < contacts.length; i++) {
+    // If the contact exists:
+    if (contacts[i].firstName && contacts[i].firstName == name) {
+        // If the property exists:
+        if (contacts[i].hasOwnProperty[prop]) {
+            // Return the property's value:
+            return(contacts[i][prop]);
+        } else {
+            return "No such property";
+        }
+    } else {
+        return "No such contact";
+    }
+}
+
+// Only change code above this line
+}
+
+// Change these values to test your function
+lookUpProfile("Akira", "likes");
