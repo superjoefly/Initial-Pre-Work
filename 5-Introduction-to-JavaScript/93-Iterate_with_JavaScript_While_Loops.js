0495f8550c21706c752f79c1089d94a4b0f6c037
JoeFly <superjoefly@gmail.com>
2019-08-28 22:58:57 -0600

    Learned how to Iterate with JS While Loops

---
+++ 5-Introduction-to-JavaScript/93-Iterate_with_JavaScript_While_Loops.js
@@ -0,0 +1,16 @@
+
+// Learned how to Iterate with JS While Loops
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+
+var num = 0;
+
+while (num < 5) {
+  myArray.push(num);
+  num++;
+}
+
+console.log(myArray);
