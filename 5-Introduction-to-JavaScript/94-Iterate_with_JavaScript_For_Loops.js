1990944bb04749c00849d7f7bbae9de8cf6c4201
JoeFly <superjoefly@gmail.com>
2019-08-28 23:02:21 -0600

    Learned how to Iterate with JS For Loops

---
+++ 5-Introduction-to-JavaScript/94-Iterate_with_JavaScript_For_Loops.js
@@ -0,0 +1,21 @@
+
+// Learned how to Iterate with JS For Loops
+
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+
+var num = 1;
+for (num; num <= 5; num++) {
+  myArray.push(num);
+}
+
+console.log(myArray);
