be69c9990d4533fca5b7803bc2cb50887265c219
JoeFly <superjoefly@gmail.com>
2019-08-28 22:14:19 -0600

    Learned how to Access Nested Arrays that Contain Objects

---
+++ 5-Introduction-to-JavaScript/91-Accessing_Nested_Arrays.js
@@ -0,0 +1,26 @@
+
+// Learned how to Access Nested Arrays that Contain Objects
+
+// Setup
+var myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+// Only change code below this line
+
+var secondTree = myPlants[1].list[1]; // Change this line
