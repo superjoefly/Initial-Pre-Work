02392fcbb7ed24930e89a6c26c982fdfd8d28403
JoeFly <superjoefly@gmail.com>
2019-08-28 23:04:47 -0600

    Learned how to Iterate Odd Numbers with a For Loop

---
+++ 5-Introduction-to-JavaScript/95-Iterate_Odd_Numbers_With_a_For_Loop.js
@@ -0,0 +1,18 @@
+
+// Learned how to Iterate Odd Numbers with a For Loop
+
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 10; i += 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+
+for (var i = 1; i <= 9; i += 2) {
+  myArray.push(i);
+}
