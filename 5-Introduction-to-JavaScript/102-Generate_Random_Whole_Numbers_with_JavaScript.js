59284adb6eaee9c64585114509fe559c8a3f5b8e
superjoefly <superjoefly@gmail.com>
2019-08-29 16:36:14 -0600

    Learned how to Generate Random Whole Numbers with JS

---
+++ 5-Introduction-to-JavaScript/102-Generate_Random_Whole_Numbers_with_JavaScript.js
@@ -0,0 +1,11 @@
+
+// Learned how to Generate Random Whole Numbers with JS
+
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+
+function randomWholeNum() {
+
+  // Only change code below this line.
+
+  return Math.floor(Math.random() * 10);
+}
