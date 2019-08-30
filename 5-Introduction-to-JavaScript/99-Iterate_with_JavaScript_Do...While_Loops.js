b302ea13936c0f05a2a3e654a31edb7c28ec8b98
JoeFly <superjoefly@gmail.com>
2019-08-29 21:57:52 -0600

    Updating code to reflect recent changes

--- 5-Introduction-to-JavaScript/99-Iterate_with_JavaScript_Do...While_Loops.js
+++ 5-Introduction-to-JavaScript/99-Iterate_with_JavaScript_Do...While_Loops.js
@@ -1,9 +1,5 @@
 
-<<<<<<< HEAD
-// Learned to Iterate with JS Do...While Loops
-=======
 // Learned how to Iterate with JS Do...While Loops
->>>>>>> 76ace5a33b3216d8334091712688a24b0866608a
 
 // Setup
 var myArray = [];
@@ -13,12 +9,5 @@ var i = 10;
 
 do {
   myArray.push(i);
-<<<<<<< HEAD
-  i++
-} while (i < 10);
-
-console.log(myArray);
-=======
   i++;
 } while (i < 10);
->>>>>>> 76ace5a33b3216d8334091712688a24b0866608a
