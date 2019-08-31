0381189f8652b9880f755782461f1f4c7f18969b
JoeFly <superjoefly@gmail.com>
2019-08-30 18:09:30 -0600

    Learned the Difference Between the var and let Keywords

---
+++ 6-Introduction-to-the-ES6-Challenges/1-Explore-Differences-Between-the-var-and-let-Keywords.js
@@ -0,0 +1,13 @@
+
+// Learned the Difference Between the var and let Keywords
+
+let catName;
+let quote;
+function catTalk() {
+  "use strict";
+
+  catName = "Oliver";
+  quote = catName + " says Meow!";
+
+}
+catTalk();
