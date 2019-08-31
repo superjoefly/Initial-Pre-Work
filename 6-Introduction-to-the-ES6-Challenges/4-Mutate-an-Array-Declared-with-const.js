85299ad5951d489fa4a9d0083c7b7ce969e2e165
JoeFly <superjoefly@gmail.com>
2019-08-30 18:20:37 -0600

    Learned how to Mutate an Array Declared with const

---
+++ 6-Introduction-to-the-ES6-Challenges/4-Mutate-an-Array-Declared-with-const.js
@@ -0,0 +1,15 @@
+
+// Learned how to Mutate an Array Declared with const
+
+const s = [5, 7, 2];
+function editInPlace() {
+  "use strict";
+  // change code below this line
+
+  s[0] = 2;
+  s[1] = 5;
+  s[2] = 7;
+
+  // change code above this line
+}
+editInPlace();
