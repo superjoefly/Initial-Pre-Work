169ea2ae6b97a6c5c58430cd470e89dc59fa3973
JoeFly <superjoefly@gmail.com>
2019-08-31 17:49:48 -0600

    Learned to use the Rest Operator with Function Parameters

---
+++ 6-Introduction-to-the-ES6-Challenges/10-Use-the-Rest-Operator-with-Function-Parameters.js
@@ -0,0 +1,12 @@
+
+// Learned to use the Rest Operator with Function Parameters
+
+const sum = (function() {
+  "use strict";
+
+  return function sum(...args) {
+    return args.reduce((a, b) => a + b, 0);
+  };
+
+})();
+console.log(sum(1, 2, 3)); // 6
