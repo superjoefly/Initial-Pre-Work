8c24c3e9339b7d84141f3d67451849ecf783a879
JoeFly <superjoefly@gmail.com>
2019-08-31 19:01:26 -0600

    Learned to use Destructuring Assignment to Assign Variables from Arrays

---
+++ 6-Introduction-to-the-ES6-Challenges/14-Use-Destructuring-Assignment-to-Assign-Variables-from-Arrays.js
@@ -0,0 +1,12 @@
+
+// Learned to use Destructuring Assignment to Assign Variables from Arrays
+
+let a = 8, b = 6;
+(() => {
+  "use strict";
+  // change code below this line
+  [a, b] = [6, 8];
+  // change code above this line
+})();
+console.log(a); // should be 6
+console.log(b); // should be 8
