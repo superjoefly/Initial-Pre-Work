8e842f8a5fbcdf860aae4dd7d2b73ff3b50e8e10
JoeFly <superjoefly@gmail.com>
2019-08-31 21:17:56 -0600

    Learned to use Class Syntax to Define a Constructor Function

---
+++ 6-Introduction-to-the-ES6-Challenges/20-Use-class-Syntax-to-Define-a-Constructor-Function.js
@@ -0,0 +1,19 @@
+
+// Learned to use Class Syntax to Define a Constructor Function
+
+function makeClass() {
+  "use strict";
+  /* Alter code below this line */
+
+  class Vegetable {
+    constructor(name) {
+      this.name = name;
+    }
+  }
+
+  /* Alter code above this line */
+  return Vegetable;
+}
+const Vegetable = makeClass();
+const carrot = new Vegetable('carrot');
+console.log(carrot.name); // => should be 'carrot'
